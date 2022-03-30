95df79c684e468d26d5beafca7ddf9f4e208f8e5
AlexBulatovKirov <bulatov@vtlkirov.ru>
2022-03-30 16:59:22 +0300

    Класс для создания авто + подкласс для автобуса

--- pyownmod.py
+++ pyownmod.py
@@ -1,15 +1,46 @@
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
-def OWMMOD():
+
+
+
+def OWMMOD1(x):
     owm = OWM('b0c99ce5cec673f9bf605ca8f8b39323')
     mgr = owm.weather_manager()
 
-    observation = mgr.weather_at_place('Kirov,RU')
+    observation = mgr.weather_at_place(x)
+    w = observation.weather
+
+    a = w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+    print(a['temp'])
+
+def OWMMOD2(x):
+        owm = OWM('b0c99ce5cec673f9bf605ca8f8b39323')
+        mgr = owm.weather_manager()
+
+        observation = mgr.weather_at_place(x)
+        w = observation.weather
+
+        a = w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+        print(a['temp_max'])
+
+
+def OWMMOD3(x):
+    owm = OWM('b0c99ce5cec673f9bf605ca8f8b39323')
+    mgr = owm.weather_manager()
+
+    observation = mgr.weather_at_place(x)
+    w = observation.weather
+
+    a = w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+    print(a['temp_min'])
+
+def OWMMOD(x):
+    owm = OWM('b0c99ce5cec673f9bf605ca8f8b39323')
+    mgr = owm.weather_manager()
+
+    observation = mgr.weather_at_place(x)
     w = observation.weather
 
     a = w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
     print(a)
-    print(a['temp'])
-    print(a['temp_max'])
-    print(a['temp_min'])
--- pyqtmod.py
+++ pyqtmod.py
@@ -1,14 +1,61 @@
 from PyQt5.QtWidgets import *
+from pyownmod import *
 app = QApplication([]) #обязательная строка
-window=QWidget() #окно
-#Создаем слой и располагаем элементы в ряд
-layout = QVBoxLayout()
-layout.addWidget(QPushButton('Первая кнопка')) #кнопка 1
-layout.addWidget(QPushButton('2')) #кнопка 2
-layout.addWidget(QPushButton('3')) #кнопка 3
-layout.addWidget(QPushButton('4')) #кнопка 4
-#Размещаем слой в окне
-window.setLayout(layout)
-# команда отображения на экране
-window.show()
+
+btn1 = QPushButton("Минимальная")
+btn2 = QPushButton("Максимальная")
+btn3 = QPushButton("Текущая")
+
+rad1 = QRadioButton('Киров')
+rad2 = QRadioButton('Москва')
+rad3 = QRadioButton('Казань')
+
+
+class Form(QWidget):
+    def initForm(self):
+        layout = QHBoxLayout()
+        layout.addWidget(btn1)
+        layout.addWidget(btn2)
+        layout.addWidget(btn3)
+        layout.addWidget(rad1)
+        layout.addWidget(rad2)
+        layout.addWidget(rad3)
+        self.setLayout(layout)
+        self.show()
+
+
+window=Form()
+window.initForm()
+
+def onRad1():
+    rad1.setChecked(True)
+    if rad1.isChecked():
+        x = 'Kirov'
+
+def onRad2():
+    rad2.setChecked(True)
+    if rad2.isChecked():
+        x = 'Moscow'
+
+def onRad3():
+    rad3.setChecked(True)
+    if rad3.isChecked():
+        x = 'Kazan'
+
+
+
+
+def on_click1():
+    OWMMOD1(x)
+def on_click2():
+    OWMMOD2(x)
+def on_click3():
+    OWMMOD3(x)
+
+btn1.clicked.connect(on_click3)
+btn2.clicked.connect(on_click2)
+btn3.clicked.connect(on_click1)
+
+
+
 app.exec_() # запустить приложение, пока пользователь не закроет его
